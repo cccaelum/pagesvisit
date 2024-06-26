1d61c01bf31326dc3f493e60216a9dad00eb0eb9
cccaelum <ccc.trad@gmail.com>
2024-06-26 11:31:34 +0200

    'review'

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const counter = document.getElementById('contadorVisitas')
 const resetBtn = document.getElementById('btnReestablecer')
 
-const countVisit = () => {
-localStorage.setItem('counter',(parseFloat(localStorage.getItem('counter')) || 0)+ 1)
+/*const countVisit = () => {
+localStorage.setItem('counter',(parseInt(localStorage.getItem('counter')) || 0)+ 1)
 console.log(localStorage.getItem('counter'))
 counter.innerHTML = localStorage.getItem('counter')
-}
+}*/
 
 const resetVisit = () => {
 localStorage.setItem('counter', 0)
@@ -13,4 +13,13 @@ counter.innerHTML = localStorage.getItem('counter')
 }
 
 resetBtn.addEventListener('click', resetVisit)
-window.addEventListener('load', countVisit)
+/*window.addEventListener('load', countVisit)*/
+
+/*Codigo de Data*/
+
+let getCountLocal = parseInt(localStorage.getItem('counter')) || 0
+
+getCountLocal++
+
+localStorage.setItem('counter', getCountLocal)
+contadorVisitas.textContent = localStorage.getItem('counter')
